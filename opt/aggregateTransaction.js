ec4212040472e1298b8f1d4b2e647a3928cd5f55
ishida_d <ishida_d@vitallead.co.jp>
2021-07-27 23:03:26 +0900

    update transaciton

--- opt/aggregateTransaction.js
+++ opt/aggregateTransaction.js
@@ -12,29 +12,29 @@ const mosaic_id = '39E0C49FA322A459'; //DHP
 const networkGenerationHash = 'ED5761EA890A096C50D3F50B7C2F0CCB4B84AFC9EA870F381E84DDE36D04EF16'  //DHP
 const networkCurrencyDivisibility = 6
 const explorer = 'http://explorer.dhealth.cloud'
+const limit = 1;  //100以上は不可
+const mosaic_limit = 100; //アラートを表示するモザイク量
 var epochAdjustment
 // ======================================
 
 // (※ここを追加する)
 // ======================================
 // 送信したいアドレス配列
-// 送信したいアドレスを増やす場合はここを増やす
-var address_list = [
-  'NABLQQ7K********************',
-  'NCGWJEOP********************',
-]
+// 送信したいアドレスを増やす場合はaddress.jsを編集
+const address_list = require('./address.js')
+
 //送信するモザイク量
-var mosaic_size = 1.1
+var mosaic_size = 0.1
 //手数料(DHPの場合0でもイケる)
 var fee_size = 0
+// address.jsにメッセージを追加するとそちらを優先します。address.jsのメッセージが空の場合はこちらのメッセージを優先します。
 var message = 'ここにメッセージを入れる'
 // ======================================
 
 // 生きているノードの取得
 async function connectNode(){
-  const nodes = nodeList
 
-  const node = nodes[Math.floor(Math.random() * nodes.length)]
+  const node = nodeList[Math.floor(Math.random() * nodeList.length)]
   const url =  node + "/node/health"
   try{
     return await axios.get(url,{timeout: 1500})
@@ -65,13 +65,21 @@ function readUserInput(question) {
   });
 }
 
+//配列を指定個数分で分割して返す関数
+function sliceByNumber (array, number) {
+  const length = Math.ceil(array.length / number)
+  return new Array(length).fill().map((_, i) =>
+    array.slice(i * number, (i + 1) * number)
+  )
+}
+
 (async() => {
   console.log('=== start ===')
 
   //ノードの取得
   const node = await connectNode()
   console.log('選択されたノード',node)
-
+  
   //リポジトリファクトリの生成
   const repositoryFactory = new symbol_sdk_1.RepositoryFactoryHttp(node)
   //epochAdjustmentの取得
@@ -86,7 +94,7 @@ function readUserInput(question) {
   const mosaicSize = mosaic_size * Math.pow(10, networkCurrencyDivisibility)
   console.log("送信するmosaic量", mosaic_size)
 
-  if(mosaic_size >= 100){
+  if(mosaic_size >= mosaic_limit){
     const ans = await readUserInput('送信するmosaic量が ' + mosaic_size + " です。ほんとに送信しますか？[yes/N] ");
     if(ans != 'yes'){
       console.log('送信を中止しました。');
@@ -94,37 +102,44 @@ function readUserInput(question) {
     }
   }
 
-  //送信先アドレス配列
-  let sendAddressList = [];
-  //送信先トランザクションを生成
-  address_list.forEach(list => {
-    console.log("送信アドレス", list)
-    const mosaic = new symbol_sdk_1.Mosaic(networkCurrencyMosaicId, symbol_sdk_1.UInt64.fromUint(mosaicSize))
-    
-    //アグリゲートトランザクションを送信するアドレスを生成
-    const address = symbol_sdk_1.Address.createFromRawAddress(list)
-    const transferTransaction = symbol_sdk_1.TransferTransaction.create(symbol_sdk_1.Deadline.create(epochAdjustment),
-      address, [mosaic], symbol_sdk_1.PlainMessage.create(message), network_type)
-    const aggregatedTransfar = transferTransaction.toAggregate(senderAccount.publicAccount)
+  //アドレスの分割
+  addressList = sliceByNumber(address_list, limit)
+  addressList.forEach(address_list => {
+    //送信先アドレス配列
+    let sendAddressList = []
+    //送信先トランザクションを生成
+    address_list.forEach(list => {
+      console.log("送信アドレス", list[0])
+      const strMsg = list[1] === "" ? message : list[1]
+      console.log("送信メッセージ", strMsg);
+      const mosaic = new symbol_sdk_1.Mosaic(networkCurrencyMosaicId, symbol_sdk_1.UInt64.fromUint(mosaicSize))
+      
+      //アグリゲートトランザクションを送信するアドレスを生成
+      const address = symbol_sdk_1.Address.createFromRawAddress(list[0])
+      const transferTransaction = symbol_sdk_1.TransferTransaction.create(symbol_sdk_1.Deadline.create(epochAdjustment),
+        address, [mosaic], symbol_sdk_1.PlainMessage.create(strMsg), network_type)
+      const aggregatedTransfar = transferTransaction.toAggregate(senderAccount.publicAccount)
 
-    //配列に保持
-    sendAddressList.push(aggregatedTransfar);
-  })
+      //配列に保持
+      sendAddressList.push(aggregatedTransfar);
+    })
 
-  //送信リストが空でなければ実行
-  if(sendAddressList.length != 0){
-    //アグリゲートトランザクションに署名
-    const aggregateTransaction = symbol_sdk_1.AggregateTransaction.createComplete(symbol_sdk_1.Deadline.create(epochAdjustment),
-      sendAddressList,
-      network_type, [], symbol_sdk_1.UInt64.fromUint(fee_size))
+    //送信リストが空でなければ実行
+    if(sendAddressList.length != 0){
+      //アグリゲートトランザクションに署名
+      const aggregateTransaction = symbol_sdk_1.AggregateTransaction.createComplete(symbol_sdk_1.Deadline.create(epochAdjustment),
+        sendAddressList,
+        network_type, [], symbol_sdk_1.UInt64.fromUint(fee_size))
 
-    //アグリゲートトランザクションをアナウンス
-    const signedTransaction = senderAccount.sign(aggregateTransaction, networkGenerationHash)
-    // replace with node endpoint
-    const transactionHttp = repositoryFactory.createTransactionRepository()
-    transactionHttp.announce(signedTransaction).subscribe((x) => {
-      console.log("success",x)
-      console.log("トランザクション詳細", explorer + "/transactions/" + signedTransaction.hash)
-    }, (err) => console.error("error",err))
-  }
+      //アグリゲートトランザクションをアナウンス
+      const signedTransaction = senderAccount.sign(aggregateTransaction, networkGenerationHash)
+      // replace with node endpoint
+      const transactionHttp = repositoryFactory.createTransactionRepository()
+      transactionHttp.announce(signedTransaction).subscribe((x) => {
+        console.log("success",x)
+        console.log("トランザクション詳細", explorer + "/transactions/" + signedTransaction.hash)
+      }, (err) => console.error("error",err))
+    }
+  });
+  
 })()
