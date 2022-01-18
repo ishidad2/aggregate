3ed88553614218e8a2229ece04ae58a09530889c
ishida_d <ishida_d@vitallead.co.jp>
2022-01-18 10:59:04 +0900

    update mosaic_num

--- opt/aggregateTransaction.js
+++ opt/aggregateTransaction.js
@@ -3,50 +3,51 @@
 require('dotenv').config()
 
 const axios = require('axios').default
-const nodeList = require('./config.js')
-const symbol_sdk_1 = require('symbol-sdk')
+const nodeList = require('./config.js');
+const symbol_sdk_1 = require('symbol-sdk');
 
-const network_type = symbol_sdk_1.NetworkType.MAIN_NET
-const private_key = process.env.CERTIFICATE_PRIVATE_KEY
+const network_type = symbol_sdk_1.NetworkType.MAIN_NET;
+const private_key = process.env.CERTIFICATE_PRIVATE_KEY;
 const mosaic_id = '39E0C49FA322A459'; //DHP
-const networkGenerationHash = 'ED5761EA890A096C50D3F50B7C2F0CCB4B84AFC9EA870F381E84DDE36D04EF16'  //DHP
-const networkCurrencyDivisibility = 6
-const explorer = 'http://explorer.dhealth.cloud'
+const networkGenerationHash = 'ED5761EA890A096C50D3F50B7C2F0CCB4B84AFC9EA870F381E84DDE36D04EF16';  //DHP
+const networkCurrencyDivisibility = 6;
+const explorer = 'http://explorer.dhealth.cloud';
 const limit = 100;  //100以上は不可
 const mosaic_limit = 100; //アラートを表示するモザイク量
-var epochAdjustment
+var epochAdjustment;
+var isMosaicLimit=false;
 // ======================================
 
 // (※ここを追加する)
 // ======================================
 // 送信したいアドレス配列
 // 送信したいアドレスを増やす場合はaddress.jsを編集
-const address_list = require('./address.js')
+const address_list = require('./address.js');
 
 //送信するモザイク量
-var mosaic_size = 0.1
+var mosaic_size = 0.1;
 //手数料(DHPの場合0でもイケる)
-var fee_size = 0
+var fee_size = 0;
 // address.jsにメッセージを追加するとそちらを優先します。address.jsのメッセージが空の場合はこちらのメッセージを優先します。
-var message = 'ここにメッセージを入れる'
+var message = 'ここにメッセージを入れる';
 // ======================================
 
 // 生きているノードの取得
 async function connectNode(){
 
-  const node = nodeList[Math.floor(Math.random() * nodeList.length)]
-  const url =  node + "/node/health"
+  const node = nodeList[Math.floor(Math.random() * nodeList.length)];
+  const url =  node + "/node/health";
   try{
     return await axios.get(url,{timeout: 1500})
     .then(function(response) {
       if(response.data.status.apiNode == "up" && response.data.status.db == "up"){
-        return node
+        return node;
       }
-      return connectNode()
+      return connectNode();
     })
   } catch (error){
-    console.log('error', error)
-    return connectNode()
+    console.log('error', error);
+    return connectNode();
   }
 }
 
@@ -67,79 +68,84 @@ function readUserInput(question) {
 
 //配列を指定個数分で分割して返す関数
 function sliceByNumber (array, number) {
-  const length = Math.ceil(array.length / number)
+  const length = Math.ceil(array.length / number);
   return new Array(length).fill().map((_, i) =>
     array.slice(i * number, (i + 1) * number)
-  )
+  );
 }
 
 (async() => {
-  console.log('=== start ===')
+  console.log('=== start ===');
 
   //ノードの取得
-  const node = await connectNode()
-  console.log('選択されたノード',node)
+  const node = await connectNode();
+  console.log('選択されたノード',node);
   
   //リポジトリファクトリの生成
-  const repositoryFactory = new symbol_sdk_1.RepositoryFactoryHttp(node)
+  const repositoryFactory = new symbol_sdk_1.RepositoryFactoryHttp(node);
   //epochAdjustmentの取得
-  epochAdjustment = await repositoryFactory.getEpochAdjustment().toPromise()
+  epochAdjustment = await repositoryFactory.getEpochAdjustment().toPromise();
 
   //送信アカウントをプライベートキーから生成
-  const senderAccount = symbol_sdk_1.Account.createFromPrivateKey(private_key, network_type)
+  const senderAccount = symbol_sdk_1.Account.createFromPrivateKey(private_key, network_type);
   // replace with mosaic_id
-  const networkCurrencyMosaicId = new symbol_sdk_1.MosaicId(mosaic_id)
+  const networkCurrencyMosaicId = new symbol_sdk_1.MosaicId(mosaic_id);
   
-  //送信するモザイク量
-  const mosaicSize = mosaic_size * Math.pow(10, networkCurrencyDivisibility)
-  console.log("送信するmosaic量", mosaic_size)
-
-  if(mosaic_size >= mosaic_limit){
-    const ans = await readUserInput('送信するmosaic量が ' + mosaic_size + " です。ほんとに送信しますか？[yes/N] ");
-    if(ans != 'yes'){
-      console.log('送信を中止しました。');
-      process.exit()
-    }
-  }
-
   //アドレスの分割
-  addressList = sliceByNumber(address_list, limit)
-  addressList.forEach(address_list => {
+  const addressList = sliceByNumber(address_list, limit);
+  for(list of addressList) {
     //送信先アドレス配列
-    let sendAddressList = []
+    let sendAddressList = [];
     //送信先トランザクションを生成
-    address_list.forEach(list => {
-      console.log("送信アドレス", list[0])
-      const strMsg = list[1] === "" ? message : list[1]
+    list.forEach(item => {
+      console.log("送信アドレス", item[0]);
+      const strMsg = item[1] === "" ? message : item[1];
+      const mosaicNum = (item[2] === 0 || item[2] === 0.0) ? mosaic_size : item[2]
       console.log("送信メッセージ", strMsg);
-      const mosaic = new symbol_sdk_1.Mosaic(networkCurrencyMosaicId, symbol_sdk_1.UInt64.fromUint(mosaicSize))
+      //送信するモザイク量
+      const mosaicSize = mosaicNum * Math.pow(10, networkCurrencyDivisibility);
+      console.log("送信するモザイク量", mosaicNum);
+
+      if(mosaicNum >= mosaic_limit && !isMosaicLimit){
+        isMosaicLimit = true;
+      }
+
+      const mosaic = new symbol_sdk_1.Mosaic(networkCurrencyMosaicId, symbol_sdk_1.UInt64.fromUint(mosaicSize));
       
       //アグリゲートトランザクションを送信するアドレスを生成
-      const address = symbol_sdk_1.Address.createFromRawAddress(list[0])
+      const address = symbol_sdk_1.Address.createFromRawAddress(item[0]);
       const transferTransaction = symbol_sdk_1.TransferTransaction.create(symbol_sdk_1.Deadline.create(epochAdjustment),
         address, [mosaic], symbol_sdk_1.PlainMessage.create(strMsg), network_type)
-      const aggregatedTransfar = transferTransaction.toAggregate(senderAccount.publicAccount)
+      const aggregatedTransfar = transferTransaction.toAggregate(senderAccount.publicAccount);
 
       //配列に保持
       sendAddressList.push(aggregatedTransfar);
-    })
+    });
+
+    if(isMosaicLimit){
+      const ans = await readUserInput('送信するmosaic量が ' + mosaic_limit + " を超えるものがあります。ほんとに送信しますか？[yes/N] ");
+      if(ans != 'yes'){
+        console.log('送信を中止しました。');
+        process.exit();
+      }
+    }
 
     //送信リストが空でなければ実行
     if(sendAddressList.length != 0){
       //アグリゲートトランザクションに署名
       const aggregateTransaction = symbol_sdk_1.AggregateTransaction.createComplete(symbol_sdk_1.Deadline.create(epochAdjustment),
         sendAddressList,
-        network_type, [], symbol_sdk_1.UInt64.fromUint(fee_size * Math.pow(10, networkCurrencyDivisibility)))
+        network_type, [], symbol_sdk_1.UInt64.fromUint(fee_size * Math.pow(10, networkCurrencyDivisibility)));
 
       //アグリゲートトランザクションをアナウンス
-      const signedTransaction = senderAccount.sign(aggregateTransaction, networkGenerationHash)
+      const signedTransaction = senderAccount.sign(aggregateTransaction, networkGenerationHash);
       // replace with node endpoint
-      const transactionHttp = repositoryFactory.createTransactionRepository()
+      const transactionHttp = repositoryFactory.createTransactionRepository();
       transactionHttp.announce(signedTransaction).subscribe((x) => {
-        console.log("success",x)
-        console.log("トランザクション詳細", explorer + "/transactions/" + signedTransaction.hash)
+        console.log("success",x);
+        console.log("トランザクション詳細", explorer + "/transactions/" + signedTransaction.hash);
       }, (err) => console.error("error",err))
     }
-  });
+  }
   
 })()
